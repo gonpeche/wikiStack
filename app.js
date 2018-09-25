fb2c23a41a3fdb7c287f65f2e676ec450a671328
gonpeche <espeche.g@gmail.com>
2018-09-25 12:16:32 -0300

    update

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-var morgan = require('morgan')
-var express = require('express')
-var app = express()
+var morgan = require('morgan');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser')
 
 
 app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+var env = nunjucks.configure('views', {noCache: true});
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+
+
+app.get('/', function(res,res) {
+    res.render('index')
+})
+
+
+app.listen(3000, () => 'Listening...')
