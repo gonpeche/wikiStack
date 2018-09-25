bd1aacf743321d0f861d14ed072942dc2b1adf0b
gonpeche <espeche.g@gmail.com>
2018-09-25 15:54:59 -0300

    update

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var morgan = require('morgan');
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser')
-var nunjucks = require('nunjucks')
+var bodyParser = require('body-parser');
+var nunjucks = require('nunjucks');
+var models = require('./models')
 
 
 app.use(express.static('public'))
@@ -20,5 +21,12 @@ app.get('/', function(res,res) {
     res.render('index')
 })
 
-
-app.listen(3000, () => 'Listening...')
+// Asegurate de estar exportando tu db del archivo de tus modelos
+models.db.sync({force: true})
+.then(function () {
+    // asegurate de reemplazar el nombre de abajo con tu app de express
+    app.listen(3000, function () {
+        console.log('Server is listening on port 3001!');
+    });
+})
+.catch(console.error);
